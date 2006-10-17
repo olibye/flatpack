f5178c5d7da3c31b041bb21fc5b9897cb7050f56
benoitx <benoitx@509fa23a-e01f-0410-8d53-9f7e05c29f80>
2006-10-17 15:05:44 +0000

    Work on the examples to use a Console menu that the user can launch and then pick which test to run interactively.
    git-svn-id: https://flatpack.svn.sourceforge.net/svnroot/flatpack/trunk@67 509fa23a-e01f-0410-8d53-9f7e05c29f80

---
+++ PZFileReaderSamples/src/main/java/com/pz/reader/examples/largedataset/delimiteddynamiccolumns/LargeDelimitedWithPZMap.java
@@ -0,0 +1,66 @@
+package com.pz.reader.examples.largedataset.delimiteddynamiccolumns;
+
+/*
+ * Created on Dec 31, 2004
+ *
+ */
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import com.pz.reader.LargeDataSet;
+
+/**
+ * @author zepernick
+ * 
+ * TODO To change the template for this generated type comment go to Window -
+ * Preferences - Java - Code Style - Code Templates
+ */
+public class LargeDelimitedWithPZMap {
+    public static void main(final String[] args) throws Exception {
+        String mapping = getDefaultMapping();
+        String data = getDefaultDataFile();
+        call(mapping, data);
+
+    }
+
+    public static String getDefaultDataFile() {
+        return "PEOPLE-CommaDelimitedWithQualifier.txt";
+    }
+
+    public static String getDefaultMapping() {
+        return "PEOPLE-Delimited.pzmap.xml";
+    }
+
+    public static void call(String mapping, String data) throws Exception {
+        LargeDataSet ds = null;
+        String[] colNames = null;
+        FileInputStream pzmap = null;
+        FileInputStream fileToParse = null;
+
+        pzmap = new FileInputStream(new File(mapping));
+        fileToParse = new FileInputStream(new File(data));
+        // delimited by a comma
+        // text qualified by double quotes
+        // ignore first record
+        ds = new LargeDataSet(pzmap, fileToParse, ",", "\"", true, false);
+
+        colNames = ds.getColumns();
+
+        while (ds.next()) {
+            for (int i = 0; i < colNames.length; i++) {
+                System.out.println("COLUMN NAME: " + colNames[i] + " VALUE: " + ds.getString(colNames[i]));
+            }
+
+            System.out.println("===========================================================================");
+        }
+
+        if (ds.getErrors() != null && ds.getErrors().size() > 0) {
+            System.out.println("FOUND ERRORS IN FILE");
+        }
+
+        // clear out the DataSet object for the JVM to collect
+        ds.freeMemory();
+
+    }
+}
---
+++ PZFileReaderSamples/src/main/java/com/pz/reader/examples/largedataset/fixedlengthdynamiccolumns/LargeFixedLengthWithPZMap.java
@@ -0,0 +1,54 @@
+package com.pz.reader.examples.largedataset.fixedlengthdynamiccolumns;
+
+/*
+ * Created on Dec 31, 2004
+ *
+ */
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import com.pz.reader.LargeDataSet;
+
+/**
+ * @author zepernick
+ * 
+ * TODO To change the template for this generated type comment go to Window -
+ * Preferences - Java - Code Style - Code Templates
+ */
+public class LargeFixedLengthWithPZMap {
+    public static void main(final String[] args) throws Exception {
+        String mapping = getDefaultMapping();
+        String data = getDefaultDataFile();
+        call(mapping, data);
+    }
+
+    public static String getDefaultDataFile() {
+        return "PEOPLE-FixedLength.txt";
+    }
+
+    public static String getDefaultMapping() {
+        return "PEOPLE-FixedLength.pzmap.xml";
+    }
+
+    public static void call(String mapping, String data) throws Exception {
+        LargeDataSet ds = null;
+        String[] colNames = null;
+
+        ds = new LargeDataSet(new FileInputStream(new File(mapping)), new FileInputStream(new File(data)), false);
+
+        colNames = ds.getColumns();
+
+        while (ds.next()) {
+            for (int i = 0; i < colNames.length; i++) {
+                System.out.println("COLUMN NAME: " + colNames[i] + " VALUE: " + ds.getString(colNames[i]));
+            }
+
+            System.out.println("===========================================================================");
+        }
+
+        // clear out the DataSet object for the JVM to collect
+        ds.freeMemory();
+
+    }
+}
